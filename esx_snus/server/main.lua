d9a7cbb8bc02c8be7b707d5a5a11b09b936fdee5
Johvu <47777577+Johvu@users.noreply.github.com>
2020-10-16 00:55:59 +0300

    Fix bugs

--- esx_snus/server/main.lua
+++ esx_snus/server/main.lua
@@ -4,11 +4,5 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterUsableItem('kiekko', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	local snus = xPlayer.getInventoryItem('kiekko')
-	
-		if snus.count > 0 then
-			TriggerClientEvent('esx_snus:startuse', source)
-		else
-			TriggerClientEvent('esx:showNotification', source, ('you dont have snus'))
-		end
+				TriggerClientEvent('esx_snus:startuse', source)
 end)
