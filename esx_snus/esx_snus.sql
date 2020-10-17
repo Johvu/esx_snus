d7d15063478ab4a32dca552e82a55fce92c3925e
Johvu <47777577+Johvu@users.noreply.github.com>
2020-10-17 21:21:10 +0300

    Update esx_snus.sql

--- esx_snus/esx_snus.sql
+++ esx_snus/esx_snus.sql
@@ -3,4 +3,4 @@ INSERT INTO `items` (`name`, `label`, `limit`) VALUES
 	('kiekko2', 'Odens puck 2 sacks', 20),
 	('kiekko3', 'Odens puck 3 sacks', 20),
 	('kiekko4', 'Odens puck 4 sacks', 20),
-	('kiekko4', 'Odens puck', 20),
+	('kiekko5', 'Odens puck', 20),
